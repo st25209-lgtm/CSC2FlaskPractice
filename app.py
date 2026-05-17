2c5551a1e1ce9b5b042274f036e73f15c6ad6723
Miro Gardiola <st25209@ormiston.school.nz>
2026-05-17 18:28:25 +1200

    Added code #2

---
+++ app.py
@@ -0,0 +1,22 @@
+import json
+
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+app = Flask(__name__)
+
+
+def load_data():
+    with open('data/flowers.json') as file:
+        flowers = json.load(file)
+    return flowers
+
+@app.route('/')
+def index():
+    flowers = load_data()
+    return render_template('index.html', flower=flowers)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
